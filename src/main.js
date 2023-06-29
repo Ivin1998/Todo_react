ac017c589dfe92d6dccbff0aa5ba9121743a41cb
Ivin_Austan <ivin.austan@capestart.com>
2023-06-29 17:58:01 +0530

    todo list updated

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import Swal from "sweetalert2";
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
 
 const Todo = () => {
   const [state, setState] = useState([]);
@@ -28,26 +31,31 @@ const Todo = () => {
   };
 
   return (
-    <div>
+    
+   <div className="row">
+    <div className="col-md-4">
+      </div>
+      <div className="col-md-6">
       <h1>Todo-list</h1>
       <form>
-        <input type="text" />
-        <input type="submit" onClick={handleChange} value="Add" />
+        <input type="text" className='form-control form-style'/>
+        <input type="submit" className="btn btn-primary submit" onClick={handleChange} value="Add" />
       </form>
-      <ol>
-        {state.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button
+      <ul>
+        {state.map((item, index) => (<li key={index}> {item}
+            <div className="remove_button">
+            <button className="btn btn-danger"
               onClick={() => {
                 removeItem(index);
               }}
             >
               Delete
             </button>
+            </div>
           </li>
         ))}
-      </ol>
+      </ul>
+    </div>
     </div>
   );
 };
